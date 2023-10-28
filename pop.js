29a48975b98b244d5df932d11487b1aa38bcbac4
Shyam Sharki <ssk66221@gmail.com>
2023-10-28 20:53:35 +0545

    push pop

---
+++ pop.js
@@ -0,0 +1,8 @@
+function popArray(arr){
+
+    arr.pop();
+    console.log(arr);
+
+
+}
+popArray([10,20,30,40,50,60]);
---
+++ push.js
@@ -0,0 +1,9 @@
+function pushArray(arr){
+
+    arr.push(90,100);
+    console.log(arr);
+     
+
+
+}
+pushArray([10,20,30,40,50,60,80]);
