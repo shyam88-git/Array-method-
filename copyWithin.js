030372ca8f44a94608d6f5d41074f5c0be11a6d1
Shyam Sharki <ssk66221@gmail.com>
2023-10-28 21:39:48 +0545

    copyWithin

---
+++ copyWithin.js
@@ -0,0 +1,16 @@
+let arr1=['a','b','c','d','e'];
+//copy index 3 value to index 0.
+
+//arr1.copyWithin(0,3);\
+
+//copy index 4 value to index 1.
+
+arr1.copyWithin(1,4);
+console.log(arr1);
+
+
+console.log(arr1);
+
+
+
+
