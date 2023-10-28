82f694c8dec7f9b9ceb486b03d08586aee577f80
Shyam Sharki <ssk66221@gmail.com>
2023-10-28 21:00:36 +0545

    shift unshift

---
+++ shift.js
@@ -0,0 +1,8 @@
+function shiftArr(arr){
+
+    arr.shift();
+    console.log(arr);
+
+
+}
+shiftArr([10,20,30,40,50,60]);
---
+++ unshift.js
@@ -0,0 +1,8 @@
+function arrUnshift(arr){
+
+    arr.unshift(70);
+    console.log(arr);
+
+
+}
+arrUnshift([10,20,30,40,50,60]);
